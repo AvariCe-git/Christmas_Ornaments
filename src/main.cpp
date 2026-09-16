887b9c13efaddb4dd574ba6fcad537052e277b65
AvariCe <39452996+AvariCe-git@users.noreply.github.com.>
2026-09-16 21:17:54 +0300

    git commit -a

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,9 @@ const int pinA = PB1;
 const int pinB = PB2;
 const int pinC = PB3;
 const int random_read = PB4;
-const int delay_timer = 75;
+
+const unsigned int delay_timer = 250;
+const int i_max = 128;
 long random_number = 0;
 
 unsigned int leds[8][3] =   {{0,0,0}
@@ -30,38 +32,49 @@ void setup(){
     TCCR0A  = ((5<<WGM00) |(2<<COM0A0)) ; // mode Phase correct PWM
     TCCR0B  = ( (1<<CS00)); // clock input don't divide
     OCR0A = 0; // now it seems to be duty cycle - initial value
-    
+
     pinMode(pin_enable,OUTPUT);
     pinMode(pinA,OUTPUT);
     pinMode(pinB,OUTPUT);
     pinMode(pinC,OUTPUT);
     pinMode(random_read,INPUT);
-    randomSeed(analogRead(random_read));
-    random_number = random_int(0);
+    
+    randomSeed((random_read));
+    random_number = random_int(0); 
 }
 
 void loop(){
+
+    int i = 0;
     
-    unsigned int i = 0, i_max = 255;
     digitalWrite(pinA,leds[random_number][0]);
     digitalWrite(pinB,leds[random_number][1]);
     digitalWrite(pinC,leds[random_number][2]);
-    for (i = 0; i < 255; i++){
+    
+    digitalWrite(pin_enable, 1);
+    
+    for (i = 0; i < i_max; i++){
         OCR0A = i;
         delayMicroseconds(delay_timer);
     }
-    for (i_max = 255; i_max > 0; i_max --){
-        OCR0A = i_max;
+    
+    for (i = i_max; i > 0; i--){
+        OCR0A = i;
         delayMicroseconds(delay_timer);
     }
+    
+    digitalWrite(pin_enable, 0);
+    
     random_number = random_int(random_number);
 }
 
 long random_int(long x){
     
     long y = random(0,8);
+    
     if (y == x){
         y = random_int(y);
     }
+
     return y;
 }
